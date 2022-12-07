6bc8e0ab9dbe2931d5d5af9aeeae45183d694c86
ashli-ann petrekin <ashpetrekin@gmail.com>
2022-12-06 20:18:15 -0500

    update

--- view.php
+++ view.php
@@ -12,10 +12,13 @@
         $id = $_GET['id'];
         $sql = "SELECT * FROM contacts WHERE id='$id'";
         $results= $conn -> query($sql);
-        $result = $results->fetch_assoc();     
+        $result = $results->fetch_assoc();
+    if ($result['type'] == 'Sales Lead'){
+        $type = "Support";
+    }else{
+        $type = "Sales Lead";
     }
-
-
+    }
    
 ?>
 
@@ -52,6 +55,13 @@
         </aside>
     
 <div class="card" style="width: 18rem;">
+<input type="submit" name="atm"value= ">Assigned to me</input>
+<button>Switch to <?php echo $type?></button>
+<?php
+    if(isset($_POST['atm'])){
+
+    }
+?>
 <img src="contact.png" alt="" style="border-radius-100%" width="50px" height="50px">
     <h1><?php echo $result["title"]."".$result["firstname"]." ".$result["lastname"] ?></h1>
     <h4><?php echo "Created on ".$result["created_at"]." by ".$result["created_by"] ?></h4>
@@ -68,22 +78,25 @@
         <p class="card-text">
             Assigned To <?php echo $result['assigned_to'];  ?>
         </p>
-    </div>
-    <div class="container">
+        <div class="container">
         <?php
             $sql = "SELECT * FROM notes where id = '$id'";
-            $results= $conn -> query($sql);
-            $result = $results->fetch_assoc();
+            $note= $conn -> query($sql);
+            $notes = $note->fetch_assoc();
             echo"<h1>Notes</h1>
-            <p>$result[comment]</p>";
+            
+            <p>$notes[comment]</p>";
             
         ?>
-        <form action="get">
-            <label>Add a note about <?php echo $result['firstname'];?><label>
-            <input type = "text"> <br>
-        </form>
 
     </div>
+        <form action="get">
+            <label>Add a note about <?php echo $result['firstname'];?><label>
+            <input type = "text"> </br>
+            <input type = "submit" value= "submit"><br>
+        </form>
+    </div>
+    
 <body>
 </div>
 </html>
