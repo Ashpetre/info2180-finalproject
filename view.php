70a8da234e5083f8dbfa6644c5e4475f094f57a7
ashli-ann petrekin <ashpetrekin@gmail.com>
2022-12-06 19:34:10 -0500

    update

--- view.php
+++ view.php
@@ -4,34 +4,86 @@
     $password = "";
     $dbname = "schema"; 
     $conn= new mysqli($host,$username,$password,$dbname);
-    if(isset($_GET['id'])){
-        $id = $_GET["id"];
-        $result = getdetails($id);
+    
 
+    if(!isset($_GET['id'])){
+        echo "<h1>please check details</h1>";
+    } else {
+        $id = $_GET['id'];
+        $sql = "SELECT * FROM contacts WHERE id='$id'";
+        $results= $conn -> query($sql);
+        $result = $results->fetch_assoc();     
     }
 
 
-function getdetails($id ){
-    $sql = "selct * from notes where id ='$id'"
-    stmt = this ->db ->prepare($sql);
-    
-}
-
+   
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="addUser.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script src="addUser.js"></script>
+    
+</head>
+<body>
+    <nav>
+    <img class="dolpin" src="dolphin.jpg" alt="Dolphin CRM" width=30px height=30px>
+        <p>DolphinCRM</p>
+    </nav>
+
+    <div class="container">
+        <aside>
+            <ul>
+            <a href="dashboard.php"><li><i class="material-icons">home</i>Home</li></a>
+                <a href="new_contact.php"><li><i class="material-icons">account_circle</i>New Contact</li></a>
+                <a href="view-users.php"><li><i class="material-icons">people_outline</i>Users</li></a>
+                <hr>
+                <a href="logout.php"><li><i class="material-icons">exit_to_app</i>Logout</li></a>
+            </ul>
+        </aside>
+    
 <div class="card" style="width: 18rem;">
+<img src="contact.png" alt="" style="border-radius-100%" width="50px" height="50px">
+    <h1><?php echo $result["title"]."".$result["firstname"]." ".$result["lastname"] ?></h1>
+    <h4><?php echo "Created on ".$result["created_at"]." by ".$result["created_by"] ?></h4>
     <div class="card-body">
         <p class="card-text">
-            Email <?php echo $_POST['email'];  ?>
+            Email <?php echo $result['email'];  ?>
         </p>
         <p class="card-text">
-            Telephone <?php echo $_POST['telephone'] ;  ?>
+            Telephone <?php echo $result['telephone'] ;  ?>
         </p>
         <p class="card-text">
-            Company <?php echo $_POST['company'];  ?>    
+            Company <?php echo $result['company'];  ?>    
         </p>
         <p class="card-text">
-            Assigned To <?php echo $_POST['assigned_to'];  ?>
+            Assigned To <?php echo $result['assigned_to'];  ?>
         </p>
     </div>
+    <div class="container">
+        <?php
+            $sql = "SELECT * FROM notes where id = '$id'";
+            $results= $conn -> query($sql);
+            $result = $results->fetch_assoc();
+            echo"<h1>Notes</h1>
+            <p>$result[comment]</p>";
+            
+        ?>
+        <form action="get">
+            <label>Add a note about <?php echo $result['firstname'];?><label>
+            <input type = "text"> <br>
+        </form>
+
+    </div>
+<body>
 </div>
+</html>
